6167a0571e58eb3a890551fac831b803bd5ee870
Dhruva Vishwanath <vishwand@purdue.edu>
2021-11-29 00:12:04 -0500

    Made all api methods give valid responses

--- project-1/server/registry.py
+++ project-1/server/registry.py
@@ -3,44 +3,47 @@ from flask import Flask, redirect,url_for, request
 app = Flask(__name__)
 
 
-@app.route('/packages/<offset>',defaults = {'<offset>' : '<1>'},methods = ['GET'])
-def getPackages():
-    return {"response":[]}
+@app.route('/packages/',defaults = {'offset' : 1})
+@app.route('/packages/<int:offset>',methods = ['GET'])
+def getPackages(offset):
+    return {"response":[offset]}
 
 @app.route('/reset', methods = ['DELETE'])
 def registryReset():
     return 'Uploading to Google Cloud Bucket'
 
 @app.route('/package/<id>', methods = ['DELETE'])
-def deletePackage():
+def deletePackage(id):
     #Delete from package id
-    pass
+    return f'Deleting package {id}'
+
 @app.route('/package/<id>', methods = ['PUT'])
-def updatePackage():
-    pass
+def updatePackage(id):
+    return f'Updating package {id}'
+
 @app.route('/package/<id>', methods = ['GET'])
-def packageRetrieve():
-    pass
+def packageRetrieve(id):
+    return f'Retrieving package {id}'
 
 @app.route('/package', methods = ['POST'])
 def packageCreate():
-    pass
+    return 'Creating package'
 
 @app.route('/authenticate', methods = ['PUT'])
 def createAuthToken():
-    pass
+    return 'Creating Auth Token'
 
 @app.route('/package/byName/<name>', methods = ['GET'])
-def getPackageByName():
-    pass
+def getPackageByName(name):
+    return f'Retrieving package {name}'
 
 @app.route('/package/byName/<name>', methods = ['DELETE'])
-def deletePackageByName():
-    pass
+def deletePackageByName(name):
+    return f'Deleting package {name}'
 
 @app.route('/package/<id>/rate', methods = ['POST'])
-def rate():
-    return 'Rate Package'
+def rate(id):
+    return f'Rating Package {id}'
 
 
 
