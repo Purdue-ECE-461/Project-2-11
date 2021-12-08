ae27f9792a776df919617f029fd5dad9fa2badc2
Dhruva Vishwanath <58185967+vishdhruva@users.noreply.github.com>
2021-12-07 23:31:26 -0500

    a

--- sqlconnector.py
+++ sqlconnector.py
@@ -9,8 +9,7 @@ import sys
 #### Establish Connetion ####
 
 def connect(dbpass,dbhost,dbname):
-    cnx = mysql.connector.connect(user='root', password='2pjHC9svdbauJxFw', host='35.229.85.24',
-                                  database='registry')
+    
 
     #### Connetion Established ####
     print("Connection Established")
